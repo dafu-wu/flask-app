ce5143afc60652d27d2451ff8a2b14856a8b0750
dafu <38685037+dafu-wu@users.noreply.github.com>
2023-09-09 14:22:33 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# simple_app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Welcome to our Flask app!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
